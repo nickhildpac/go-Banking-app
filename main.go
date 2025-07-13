38343298d4be8f07b97c1974afb647e6e985a53e
nickhildpac <162016801+nickhildpac@users.noreply.github.com>
2025-07-13 05:42:15 +0000

    added account api routes and env variables

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"database/sql"
+	"log"
+	"github.com/nickhildpac/simplebank/db/sqlc"
+	"github.com/nickhildpac/simplebank/api"
+	"github.com/nickhildpac/simplebank/util"
+
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
